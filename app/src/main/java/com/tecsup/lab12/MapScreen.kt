2d5bfbf5eaf6b208229a148fd9c74ba46e334222
Fabio O <fortiz@unsa.edu.pe>
2025-06-11 00:35:11 -0500

    Integrar animación de cámara, múltiples marcadores y polígonos representando zonas de interés en Arequipa

--- app/src/main/java/com/tecsup/lab12/MapScreen.kt
+++ app/src/main/java/com/tecsup/lab12/MapScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptor
@@ -16,6 +17,7 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 
@@ -27,13 +29,14 @@ fun MapScreen(modifier: Modifier = Modifier) {
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(arequipaLocation, 12f)
     }
+
+    // Animar cámara a Yura al iniciar
     LaunchedEffect(Unit) {
         cameraPositionState.animate(
-            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984,-71.6836503), 12f), // Mover a Yura
+            update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984, -71.6836503), 12f),
             durationMs = 3000
         )
     }
-
 
     val locations = listOf(
         LatLng(-16.433415, -71.5442652), // JLByR
@@ -41,12 +44,33 @@ fun MapScreen(modifier: Modifier = Modifier) {
         LatLng(-16.3524187, -71.5675994)  // Zamacola
     )
 
+    val mallAventuraPolygon = listOf(
+        LatLng(-16.432292, -71.509145),
+        LatLng(-16.432757, -71.509626),
+        LatLng(-16.433013, -71.509310),
+        LatLng(-16.432566, -71.508853)
+    )
+
+    val parqueLambramaniPolygon = listOf(
+        LatLng(-16.422704, -71.530830),
+        LatLng(-16.422920, -71.531340),
+        LatLng(-16.423264, -71.531110),
+        LatLng(-16.423050, -71.530600)
+    )
+
+    val plazaDeArmasPolygon = listOf(
+        LatLng(-16.398866, -71.536961),
+        LatLng(-16.398744, -71.536529),
+        LatLng(-16.399178, -71.536289),
+        LatLng(-16.399299, -71.536721)
+    )
+
     Box(modifier = modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Marcador personalizado principal (Arequipa)
+            // Marcador personalizado principal
             Marker(
                 state = rememberMarkerState(position = arequipaLocation),
                 icon = bitmapDescriptorFromVector(context, R.drawable.montana),
@@ -61,6 +85,26 @@ fun MapScreen(modifier: Modifier = Modifier) {
                     snippet = "Punto de interés"
                 )
             }
+
+            // Polígonos
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue.copy(alpha = 0.3f),
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue.copy(alpha = 0.3f),
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue.copy(alpha = 0.3f),
+                strokeWidth = 5f
+            )
         }
     }
 }
