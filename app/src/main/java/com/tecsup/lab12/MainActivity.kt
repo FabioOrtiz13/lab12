b28cc88dce05a3d25b3247b25d584db2a4000f1d
Fabio O <fortiz@unsa.edu.pe>
2025-06-10 23:53:48 -0500

    Actualizar MainActivity para mostrar MapScreen dentro de Scaffold con tema y padding

--- app/src/main/java/com/tecsup/lab12/MainActivity.kt
+++ app/src/main/java/com/tecsup/lab12/MainActivity.kt
@@ -7,41 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.tecsup.lab12.ui.theme.Lab12Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Lab12Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MapScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab12Theme {
-        Greeting("Android")
     }
 }
